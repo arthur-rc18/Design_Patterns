b77ab537e4e1b6388b6267b6506dccfa072c2c94
Arthur <arthurrodri015@gmail.com>
2022-01-30 21:10:18 -0300

    Adding the concrete observers class

--- Observer_pattern/src/main/java/strategy_pattern/observers/Enemy.java
+++ Observer_pattern/src/main/java/strategy_pattern/observers/Enemy.java
@@ -1,5 +1,22 @@
 package strategy_pattern.observers;
 
-public class Enemy {
+import strategy_pattern.personagens.Personagem;
 
+public class Enemy extends Personagem{
+    private Personagem p1 ;
+    private Enemy e1;
+
+    public Enemy(int x, int y, String nome){
+        //super(x,y);
+    }
+
+    public void ataca(Personagem p1, int ataque){
+        p1.setLife(p1.getLife() - ataque);
+    }
+
+    public void distAtaca(Enemy e, Personagem p, int ataque){
+        if(e.getX() == p.getX() && e.getY() == p.getY()){
+            e.ataca(p,ataque);
+        }
+    }
 }
